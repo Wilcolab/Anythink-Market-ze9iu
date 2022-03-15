45c059c20b0742dd0b936725101f3170a40ebdae
Niv Richter <nivrichter@Nivs-MacBook-Pro.local>
2022-03-15 15:56:52 +0200

    added seeding script

---
+++ backend/seeding/seeds.js
@@ -0,0 +1,58 @@
+const seeder = require('mongoose-seed');
+seeder.connect(process.env.MONGODB_URI, function() {
+seeder.loadModels([
+    'models/User.js',
+    'models/Item.js',
+    'models/Comment.js'
+
+  ]);
+ 
+
+ 
+const users = []
+const items = []
+const comments = []
+
+for (let i=0 ; i<10; i++){
+    users.push({
+        'username': `username${i}`,
+        'email': `email${i}@email.com`,
+        'bio': `bio${i}`
+    })
+    items.push(
+        {'title': `item${i}`,
+            'description': `description${i}`,
+            'body': "body",
+            'tagList': ["Pikachu"]
+            }
+    )
+    comments.push(
+    {
+        id: i,
+        body: `comment${i}`,
+    })
+
+}
+const data = [
+    {
+        'model': 'User',
+        'documents': users
+        
+    },
+    {
+        'model': 'Item',
+        'documents': items
+    },
+    {
+        'model':'Comment',
+        'documents':comments
+    }
+];
+
+seeder.clearModels(['User','Item', 'Comment'], function() { 
+    seeder.populateModels(data, function() {
+      seeder.disconnect();
+    });
+ 
+  });
+});
