7a16c5414852c3e271d73641af06916fba766e08
Niv Richter <nivrichter@Nivs-MacBook-Pro.local>
2022-03-17 12:04:09 +0200

    removed logging of ping

--- backend/routes/api/ping.js
+++ backend/routes/api/ping.js
@@ -8,7 +8,6 @@ router.get("/",
   asyncHandler(async (req, res) => {
 
     try {
-      console.log('pingged')
       const result = await sendEvent('ping')
       return res.json(result);
     } catch (e) {
